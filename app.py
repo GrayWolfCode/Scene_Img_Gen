13a07549f95010ca09e9556c78ddba2c35d4b903
GrayWolfCode <jlee888624@gmail.com>
2023-10-02 10:22:48 +0900

    aas

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from PIL import Image, PngImagePlugin
 
 app = Flask(__name__)
 CORS(app)
-URL = 'https://81a6fde39b09e20593.gradio.live'
+URL = 'https://9fb6c315d77b3c4431.gradio.live'
 
 # Load environment variables
 PROJECT_ID = os.environ.get('PROJECT_ID')
