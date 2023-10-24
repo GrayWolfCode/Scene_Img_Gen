55c4cee849e2cd4f49d8aeda1d6923b550cbd7e8
GrayWolfCode <111516040+GrayWolfCode@users.noreply.github.com>
2023-10-24 15:56:27 +0900

    alone last

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from PIL import Image, PngImagePlugin
 
 app = Flask(__name__)
 CORS(app)
-URL = 'https://589718ad53cb5de22a.gradio.live'
+URL = 'https://6e83d44bb53f37fac5.gradio.live'
 
 # Load environment variables
 PROJECT_ID = os.environ.get('PROJECT_ID')
@@ -99,48 +99,6 @@ def generate_images():
                 image_urls.append(image_url)
 
     return jsonify({"image_urls": image_urls})
-
-@app.route('/generate_back', methods=['POST'])
-def generate_back():
-    prompt_response = request.json.get('prompt')
-    img_url_response = request.json.get('imgurl')
-    drawing_style = request.json.get('style')
-    img_response = requests.get(img_url_response, stream=True)
-    with open("input.png", 'wb') as file:
-        for chunk in img_response.iter_content(chunk_size=8192):
-            file.write(chunk)
-    if drawing_style == "Pen Sketch":
-        prompt = 'vvvsketch, ' + prompt_response + ' <lora:vvvsketch:1>'
-    else:
-        prompt = 'watercolor (medium), ' + prompt_response + ' <lora:nanase_v1:1>'
-    # img_response = requests.get(img_url_response)
-    # image_data = img_response.content
-    # base64_encoded = base64.b64encode(image_data).decode('utf-8')
-    with open('./input.png', 'rb') as file:
-        image_data = file.read()
-    encoded_image = base64.b64encode(image_data).decode('utf-8')
-    print(encoded_image)
-    payload={
-        "init_images": [encoded_image],
-        "prompt": prompt,
-        "width": 1344,
-        "height": 786,
-        "sampler_name": "Euler a",
-        "steps": 40,
-        "cfg_scale": 7
-    }
-    response = requests.post(url=f'{URL}/sdapi/v1/img2img', json=payload)
-    r = response.json()
-    for i in r['images']:
-        image = Image.open(io.BytesIO(base64.b64decode(i.split(",", 1)[0])))
-        png_payload = {"image": "data:image/png;base64," + i}
-        response2 = requests.post(url=f'{URL}/sdapi/v1/png-info', json=png_payload)
-        pnginfo = PngImagePlugin.PngInfo()
-        pnginfo.add_text("parameters", response2.json().get("info"))
-        file_name = f'output_{int(time.time())}.png'
-        image.save(file_name, pnginfo=pnginfo)
-        image_url = upload_to_firebase(file_name)
-    return jsonify({"image_urls": image_url})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
